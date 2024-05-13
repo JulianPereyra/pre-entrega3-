9bd45f8f630a6e860d4d019bc120a87b11964215
JulianPereyra <julian.pereyra1717@gmail.com>
2024-05-13 20:34:50 -0300

    cambios admin

--- Musica/Integrantes/admin.py
+++ Musica/Integrantes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+admin.site.register(models.GeneroMusical)
+admin.site.register(models.Musico)
+admin.site.register(models.Banda)
+admin.site.register(models.Pertenece)
+admin.site.register(models.SolicitudUnirseBanda)
