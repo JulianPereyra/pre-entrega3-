396300cf7e4ed7c3813b6612619532f68cd69ed2
JulianPereyra <julian.pereyra1717@gmail.com>
2024-05-14 18:52:04 -0300

    Funciona hasta aca

--- Musica/Integrantes/urls.py
+++ Musica/Integrantes/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = "Integrantes"
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('', views.pagina_principal, name='pagina_principal'),
+    path('musicos/', views.lista_musicos, name='lista_musicos'),
+    path('bandas/', views.lista_bandas, name='lista_bandas'),
+    path('generos/', views.lista_generos, name='lista_generos'),
+    # 
 ]
--- Musica/Integrantes/views.py
+++ Musica/Integrantes/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
-
 from . import models
+from django.shortcuts import render
+from .models import Musico, Banda, GeneroMusical
 
-def index(request):
-    consulta = models.Musico.objects.all
-    contexto = {"Musicos": consulta}
-    return render(request, "Integrantes/index.html", contexto)
+def pagina_principal(request):
+    musicos = Musico.objects.all()
+    bandas = Banda.objects.all()
+    generos = GeneroMusical.objects.all()
+    return render(request, 'integrantes/pagina_principal.html', {'musicos': musicos, 'bandas': bandas, 'generos': generos})
+
+def lista_musicos(request):
+    musicos = Musico.objects.all()
+    return render(request, 'integrantes/lista_musicos.html', {'musicos': musicos})
+
+def lista_bandas(request):
+    bandas = Banda.objects.all()
+    return render(request, 'integrantes/lista_bandas.html', {'bandas': bandas})
+
+def lista_generos(request):
+    generos = GeneroMusical.objects.all()
+    return render(request, 'integrantes/lista_generos.html', {'generos': generos})
+
